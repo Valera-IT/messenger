e88ed8307932ba77e626697843fc69c96428de02
Valera <valera.mahnovich@aiesec.net>
2022-02-09 18:05:52 +0300

    Add the Material Drawer Side Menu

--- app/src/main/java/com/example/messenger/MainActivity.kt
+++ app/src/main/java/com/example/messenger/MainActivity.kt
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import com.example.messenger.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.AccountHeader
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
-import com.mikepenz.materialdrawer.widget.AccountHeaderView
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
-    private lateinit var mDrawer: PrimaryDrawerItem
-    private lateinit var mHeader: AccountHeaderView
+    private lateinit var mDrawer: Drawer
+    private lateinit var mHeader: AccountHeader
     private lateinit var mToolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,11 +26,44 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        initFieleds()
+        initFields()
+        initFunc()
+    }
+
+    private fun initFunc() {
+        setSupportActionBar(mToolbar)
+        createHeader()
+        createDrawer()
+    }
+
+    private fun createDrawer() {
+        mDrawer = DrawerBuilder()
+            .withActivity(this)
+            .withToolbar(mToolbar)
+            .withActionBarDrawerToggle(true)
+            .withSelectedItem(-1)
+            .withAccountHeader(mHeader)
+            .addDrawerItems(
+                PrimaryDrawerItem().withIdentifier(100)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать группу")
+                    .withSelectable(false)
+            ).build()
+
+    }
+
+    private fun createHeader() {
+        mHeader = AccountHeaderBuilder()
+            .withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem().withName("Yura Pterov")
+                    .withEmail("+7911111111")
+            ).build()
     }
 
 
-    private fun initFieleds() {
+    private fun initFields() {
         mToolbar = mBinding.mainToolbar
     }
 }
