8bf2c11b56293069b0d1f908820263549dfb7087
Valera <valera.mahnovich@aiesec.net>
2022-02-21 00:14:31 +0300

    add settings_action_menu on SettingsFragment

--- app/src/main/java/com/example/messenger/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/messenger/ui/fragments/SettingsFragment.kt
@@ -1,5 +1,7 @@
 package com.example.messenger.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.example.messenger.R
 
 
@@ -7,5 +9,10 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu,menu)
     }
 }
