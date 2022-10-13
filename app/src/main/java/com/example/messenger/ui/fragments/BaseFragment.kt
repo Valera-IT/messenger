ffd8cb009d832a4d636dc646ad9b077e5a5c6854
Valera <valera.mahnovich@aiesec.net>
2022-10-13 23:54:37 +0300

    refactor BaseFragment.kt

--- app/src/main/java/com/example/messenger/ui/fragments/BaseFragment.kt
+++ app/src/main/java/com/example/messenger/ui/fragments/BaseFragment.kt
@@ -1,23 +1,10 @@
 package com.example.messenger.ui.fragments
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 
-open class BaseFragment(val layout: Int) : Fragment() {
+open class BaseFragment(layout: Int) : Fragment(layout) {
 
-    private lateinit var mRootView: View
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        mRootView = inflater.inflate(layout, container, false)
-        return mRootView
-    }
 
     override fun onStart() {
         super.onStart()
