a4ed3e09e219f78360a8fc5766ce105cc2b36ea8
Valera <valera.mahnovich@aiesec.net>
2022-10-13 23:53:08 +0300

    refactor toast on EnterPhoneNumberFragment.kt

--- app/src/main/java/com/example/messenger/ui/fragments/EnterPhoneNumberFragment.kt
+++ app/src/main/java/com/example/messenger/ui/fragments/EnterPhoneNumberFragment.kt
@@ -1,9 +1,9 @@
 package com.example.messenger.ui.fragments
 
 import androidx.fragment.app.Fragment
-import android.widget.Toast
 
 import com.example.messenger.R
+import com.example.messenger.utilits.showToast
 import kotlinx.android.synthetic.main.fragment_enter_phone_number.*
 
 
@@ -15,11 +15,7 @@ class EnterPhoneNumberFragment : Fragment(R.layout.fragment_enter_phone_number) 
 
     private fun sendCode() {
         if (register_input_phone_number.text.toString().isEmpty()) {
-            Toast.makeText(
-                activity,
-                getString(R.string.register_toast_enter_phone),
-                Toast.LENGTH_SHORT
-            ).show()
+            showToast(getString(R.string.register_toast_enter_phone))
         } else {
             fragmentManager?.beginTransaction()
                 ?.replace(R.id.registerDataContainer, EnterCodeFragment())
